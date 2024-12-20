db55b0275cd7f3c5a724c7b56751fdaae8428e11
Sotnicz <mikhail.selesnev2601@gmail.com>
2024-12-20 19:17:54 +0300

    11

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = int(input())
-for i in range(n):
-    a = int(input())
-    min_1 = int(input())
-    max_1 = int(input())
-    if a in min_1 and max_1:
-        print(a)
-        print()
+from Grom import Grom
+
+example = Grom()
+example.main_menu()
+
