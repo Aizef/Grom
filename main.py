c70a628bca313cebf59e1bae4f7233a6b3d2cc98
Azef <mikhail.selesnev2601@gmail.com>
2025-02-11 16:44:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ if __name__ == "__main__":
     user32 = ctypes.windll.user32
     user32.SetProcessDPIAware()
     #  установка шрифта
-    print(25 * int(screen_list[s['screen_status']].split('x')[0][:-1])
-                  * int(screen_list[s['screen_status']].split('x')[1]) // user32.GetSystemMetrics(0) // user32.GetSystemMetrics(1),
-          file=open('resources/settings/font_settings/font_size.txt', 'w'))
     
     if s['last_user'] == getpass.getuser():  #  проврка пользователя
+        print(25 * int(screen_list[s['screen_status']].split('x')[0][:-1])
+              * int(screen_list[s['screen_status']].split('x')[1]) // user32.GetSystemMetrics(
+            0) // user32.GetSystemMetrics(1),
+              file=open('resources/settings/font_settings/font_size.txt', 'w'))
         Gr = Grom(int(screen_list[s['screen_status']].split('x')[0][:-1])
                   , int(screen_list[s['screen_status']].split('x')[1])
                   )  #  создание окна на основе имеющихся данных
@@ -26,6 +27,7 @@ if __name__ == "__main__":
     else:
         user32 = ctypes.windll.user32
         user32.SetProcessDPIAware()
+        print(25, file=open('resources/settings/font_settings/font_size.txt', 'w'))
         for i in range(len(screen_list)): #  установка настроек на базовые значения
             if int(screen_list[i].split('x')[0][:-1]) == user32.GetSystemMetrics(0):
                 json.dump({"fps_status": "False", "fullscreen_status": 'True',
